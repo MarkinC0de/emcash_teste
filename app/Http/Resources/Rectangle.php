2de219701052bbd4d9d0bc51179f4bfb42036462
Marcos Gabriel <jecabyte2@hotmail.com>
2022-03-30 18:02:09 -0300

    [added] rectangle resource

---
+++ app/Http/Resources/Rectangle.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Rectangle extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'area' => $this->area,
+            'length' => $this->length,
+            'width' => $this->width,
+            'created_at' => $this->created_at->format('m/d/Y'),
+            'updated_at' => $this->updated_at->format('m/d/Y'),
+        ];
+    }
+}

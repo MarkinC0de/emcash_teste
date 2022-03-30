8f0d3d1b1bf2e73c6a09e449f71ce60bbe2d5fae
Marcos Gabriel <jecabyte2@hotmail.com>
2022-03-30 18:05:44 -0300

    [added] triangle resource

--- app/Http/Resources/Triangle.php
+++ app/Http/Resources/Triangle.php
@@ -5,7 +5,6 @@ namespace App\Http\Resources;
 use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
-use JsonSerializable;
 
 class Triangle extends JsonResource
 {

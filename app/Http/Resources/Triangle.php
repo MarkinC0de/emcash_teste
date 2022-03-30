da0e45e7d7e5de062c329e5dfe8e3f47f646f50c
Marcos Gabriel <jecabyte2@hotmail.com>
2022-03-30 18:04:13 -0300

    [added] triangle resource

---
+++ app/Http/Resources/Triangle.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use JsonSerializable;
+
+class Triangle extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'area' => $this->area,
+            'base' => $this->base,
+            'height' => $this->height,
+            'created_at' => $this->created_at->format('m/d/Y'),
+            'updated_at' => $this->updated_at->format('m/d/Y'),
+        ];
+    }
+}

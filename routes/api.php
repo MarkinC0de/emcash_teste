19d0634483ea3d687d6952ca51d08f2f5271772a
Marcos Gabriel <jecabyte2@hotmail.com>
2022-03-31 21:17:11 -0300

    [added] api routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\CalculateAreaController;
+use App\Http\Controllers\RectangleController;
+use App\Http\Controllers\TriangleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +16,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(RectangleController::class)->group(function () {
+    Route::get('/rectangle', 'index');
+    Route::post('/rectangle', 'store');
+    Route::delete('/rectangle/{rectangle}', 'destroy');
+});
+
+Route::controller(TriangleController::class)->group(function () {
+    Route::get('/triangle', 'index');
+    Route::post('/triangle', 'store');
+    Route::delete('/triangle/{triangle}', 'destroy');
+});
+
+Route::controller(CalculateAreaController::class)->group(function () {
+    Route::get('/calculate-rectangle/{rectangle}', 'calculateAreaRectangle');
+    Route::get('/calculate-triangle/{triangle}', 'calculateAreaTriangle');
 });
